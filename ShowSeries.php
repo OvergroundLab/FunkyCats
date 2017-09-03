c66d5e7a2daef84444ec1f673d9c6098f1f2185e
OvergroundLab <barryhemans@barrys-air.lan>
2017-09-03 16:24:40 +0100

    Series page links

--- ShowSeries.php
+++ ShowSeries.php
@@ -15,11 +15,18 @@ MM_reloadPage(true);
 </head>
 
 <?php
+    $series = $_GET["series"];
+    $folder = "Series/".$series."/";
+    $count = count(glob($folder."*"));
     include 'MenuBar.html';
     ?>
 
 <?php
-    for ($i=0;$i<5;$i++){
+    for ($i=0;$i<$count;$i++){
+        $folder2 = "Series/".$series."/Issue".($i+1)."/";
+        echo $folder2;
+        $pageCount = count(glob($folder2."*"));
+        echo $pageCount;
     ?>
 <div id="SeriesBlock" style="position:absolute; left:264px; top:<?php echo (359+($i*453));?>px; width:935px; height:413px; z-index:10; background-color: #000000; layer-background-color: #000000; border: 1px none #000000; visibility: visible">
 
@@ -29,7 +36,7 @@ MM_reloadPage(true);
     <div id="Layer9" style="position:absolute; left:294px; top:44px; width:612px; height:332px; z-index:47">
         <table width="600" border="0" cellspacing="0" cellpadding="0">
 <?php
-    for ($p=0;$p<30;$p++){
+    for ($p=0;$p<$pageCount;$p++){
         if($p%5==0)
         {
             if(p!=0)
@@ -41,28 +48,31 @@ MM_reloadPage(true);
     ?>
 
                 <td>
-                    <div align="center"><font color="#FFFFFF" face="Verdana, Arial, Helvetica, sans-serif"><b><a href="a_study_in_scars_page01.html">PAGE
-                        01</a></b></font></div>
+                    <div align="center"><font color="#FFFFFF" face="Verdana, Arial, Helvetica, sans-serif"><b><a href="ShowComic.php?series=<?php echo $series?>&issue=<?php echo $i+1?>&page=<?php echo $p+1?>">PAGE
+                        <?php echo ($p+1)?></a></b></font></div>
                 </td>
 <?php
     }
     ?>
         </table>
     </div>
+
+<div id="Layer7" style="position:absolute; left:37px; top:33px; width:251px; height:355px; z-index:46"><img src="astudyinscarscover.jpg" width="250" height="354"></div>
+<div id="Layer14" style="position:absolute; left:305px; top:270px; width:586px; height:84px; z-index:49">
+<p><font color="#FFFFFF" face="Verdana, Arial, Helvetica, sans-serif" size="2"><b>Credits:</b></font><br>
+<font color="#FFFFFF" face="Verdana, Arial, Helvetica, sans-serif" size="2">Story
+and art: Angelo Pizarro<br>
+Editing help: Erika<br>
+Fonts used: <i>ACME Secret Agent BB</i> by Blambot Comic Fonts, <i>Brown Shoes</i>
+by Imagex, <i>Tekton Pro</i>, <i>Verdana</i> and <i>AR Bonnie</i>.</font></p>
+</div>
+
 </div>
 
 <?php
     }
     ?>
 
-<div id="Layer7" style="position:absolute; left:301px; top:392px; width:251px; height:355px; z-index:46"><img src="astudyinscarscover.jpg" width="250" height="354"></div>
-<div id="Layer14" style="position:absolute; left:569px; top:629px; width:586px; height:84px; z-index:49"> 
-  <p><font color="#FFFFFF" face="Verdana, Arial, Helvetica, sans-serif" size="2"><b>Credits:</b></font><br>
-    <font color="#FFFFFF" face="Verdana, Arial, Helvetica, sans-serif" size="2">Story 
-    and art: Angelo Pizarro<br>
-    Editing help: Erika<br>
-    Fonts used: <i>ACME Secret Agent BB</i> by Blambot Comic Fonts, <i>Brown Shoes</i> 
-    by Imagex, <i>Tekton Pro</i>, <i>Verdana</i> and <i>AR Bonnie</i>.</font></p>
-</div>
+
 </body>
 </html>
